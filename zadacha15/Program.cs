779b65505b67c57978e2e08932221dda5399e0a7
Medet <medetnurm@gmail.com>
2022-09-04 07:17:41 +0600

    zadacha15

---
+++ zadacha15/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую 
+день недели, и проверяет, является ли этот день выходным. */
+
+int day = int.Parse(Console.ReadLine());
+if (day == 1 || day == 2 || day == 3 || day == 4 || day == 5) {
+    Console.WriteLine("нет");
+}
+else if (day == 6 || day == 7) {
+    Console.WriteLine("да");
+}
