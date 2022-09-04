cf9079c89289b3b72828810f8b561123bf752960
Medet <medetnurm@gmail.com>
2022-09-04 07:10:43 +0600

    zadacha13

---
+++ zadacha13/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int number = int.Parse(Console.ReadLine());
+
+if (number / 100 != 0) {
+    Console.WriteLine(number % 10);
+}
+else {
+    Console.WriteLine("третьей цифры нет");
+}
